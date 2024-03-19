33fbff06198fe99bbafb568ff48dc8969defd13e
Andres Rios Tascon <ariostas@gmail.com>
2024-03-19 18:34:41 -0400

    feat: re-export main structs at the root

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,10 @@ pub mod cache;
 pub mod factorial;
 pub mod polynomial;
 pub mod semigroup;
+
+// Re-export the main structs
+pub use cache::NumCache;
+pub use polynomial::{
+    prettyprint::PrettyPrintPolynomial, properties::PolynomialProperties, Polynomial,
+};
+pub use semigroup::Semigroup;
